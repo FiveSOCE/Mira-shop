20ad018520d704246f4be49db86fbe45a1b0ae16
FiveS <sgmmsionide@gmail.com>
2026-09-03 06:04:16 +1000

    Add MiraShop text utility

---
+++ src/main/java/com/mira/shop/util/Text.java
@@ -0,0 +1,10 @@
+package com.mira.shop.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public final class Text {
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+    private Text() {}
+    public static Component c(String text) { return LEGACY.deserialize(text == null ? "" : text); }
+}
