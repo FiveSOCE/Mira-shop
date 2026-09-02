7016aecee2266454e06677061db3df24d70386f3
FiveS <sgmmsionide@gmail.com>
2026-09-03 06:02:22 +1000

    Add MiraShop Java 21 Paper build

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+}
+
+group = "com.mira"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude(group = "org.bukkit", module = "bukkit")
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    archiveFileName.set("MiraShop-${project.version}.jar")
+}
